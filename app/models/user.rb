c82f05d3fc88c50181e658d16a02f9d6c50d9428
Drewisatlas <drewisatlas@gmail.com>
2019-03-01 22:24:20 -0500

    added secure passwords and unique username validation for the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   has_many :synthesizers
   has_many :user_synthesizers
   has_many :synthesizers, through: :user_synthesizers
+
+  has_secure_password
+
+  validates :username, uniqueness: {
+    case_sensitive: false
+  }
 end
